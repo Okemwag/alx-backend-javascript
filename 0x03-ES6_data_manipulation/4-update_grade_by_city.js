6501ad9fd5ba283ad4fb1bcc063ab1cdeebbeea5
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-02-27 17:40:01 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -10,7 +10,7 @@
  *   studentId: Number,
  *   grade: Number,
  * }[]} newGrades - The new grades to be given to a student.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Okemwag
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
