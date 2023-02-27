a6d18235e6437445a63c9d92de287cf1ac9afdb1
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-02-27 17:40:41 +0300

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -5,7 +5,7 @@
  *   firstName: String,
  *   location: String
  * }[]} students - The list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Okemwag
  * @returns {Number}
  */
 export default function getStudentIdsSum(students) {
