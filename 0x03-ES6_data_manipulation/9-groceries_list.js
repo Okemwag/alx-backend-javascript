b93bbddfa20b9a13f0ccc114df26dc71b9d993c6
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-02-27 17:36:23 +0300

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a map of grocery names and their quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Okemwag
  * @returns {Map<string, Number>}
  */
 export default function groceriesList() {
