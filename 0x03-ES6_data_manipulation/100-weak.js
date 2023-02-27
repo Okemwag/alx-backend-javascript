aa8d9ed4046809c62e45ba4f1891ee84b3e364e0
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-02-27 17:42:19 +0300

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -14,7 +14,7 @@ const MAX_ENDPOINT_CALLS = 5;
  *   protocol: String,
  *   name: String,
  * }} endpoint - The endpoint to make a request to.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Okemwag
  */
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
