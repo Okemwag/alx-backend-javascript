fb72f68962f6e0d312427a3dd14b8dfe137447ed
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-02-27 17:37:55 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@
  * @param {Set<String>} set - A collection of strings.
  * @param {String} startString - The string to strip from the beginning
  * of each item in the set.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Okemwag 
  * @returns {String}
  */
 export default function cleanSet(set, startString) {
