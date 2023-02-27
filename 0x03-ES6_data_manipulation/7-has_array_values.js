4546ec6dbfee861c5844a675b3d5bf7d26adca20
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-02-27 17:37:12 +0300

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,7 +2,7 @@
  * Checks if a set contains each element in an array.
  * @param {Set} set - The collection of unique items.
  * @param {*} array - The array of items.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Okemwa 
  * @returns {Boolean}
  */
 export default function hasValuesFromArray(set, array) {
