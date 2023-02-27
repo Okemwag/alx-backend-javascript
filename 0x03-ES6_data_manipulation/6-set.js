2dfaa323738d3125d9d674c1962eb8ff7af5e900
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-02-27 17:38:40 +0300

    Update 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Okemwag
  * @returns {Set}
  */
 export default function setFromArray(array) {
