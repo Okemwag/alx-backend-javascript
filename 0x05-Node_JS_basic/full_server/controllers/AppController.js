16f84e9003fe9bba258f042086c73a9edae0703b
Okemwag <gabrielokemwa83@gmail.com>
2023-03-24 14:36:43 +0300

    New Modifications

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,7 +1,4 @@
-/**
- * Contains the miscellaneous route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
+
 class AppController {
   static getHomepage(request, response) {
     response.status(200).send('Hello Holberton School!');
