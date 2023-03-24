cbe16b78cb32ee0e2104ea0f61034e21775f7c7c
Okemwag <gabrielokemwa83@gmail.com>
2023-03-24 14:32:55 +0300

    New Modifications

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ 
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
